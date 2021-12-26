6fea458413109315de606462f1ec9a8e50b443d5
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-12-26 22:08:28 +0800

    Update summary.py

--- summary.py
+++ summary.py
@@ -4,7 +4,7 @@
 from nets.yolact import yolact
 
 if __name__ == "__main__":
-    model = yolact([544, 544, 3], 81, mode="predict")
+    model = yolact([544, 544, 3], 81, train_mode=False)
     model.summary()
 
     # for i,layer in enumerate(model.layers):
