1b444f488505cc9aa47120692aac3aa61abe2990
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2021-12-26 21:53:32 +0800

    Update resnet.py

--- nets/resnet.py
+++ nets/resnet.py
@@ -25,7 +25,6 @@ def identity_block(input_tensor, kernel_size, filters, name=""):
     x = Activation('relu')(x)
     return x
 
-
 def conv_block(input_tensor, kernel_size, filters, strides=(2, 2), name=""):
     filters1, filters2, filters3 = filters
 
@@ -48,26 +47,30 @@ def conv_block(input_tensor, kernel_size, filters, strides=(2, 2), name=""):
     x = Activation('relu')(x)
     return x
 
-
 def ResNet50(inputs):
+    # 544, 544, 3 -> 272, 272, 64
     x = ZeroPadding2D((3, 3))(inputs)
     x = Conv2D(64, (7, 7), strides=(2, 2), name='backbone.conv1',use_bias=False)(x)
     x = BatchNormalization(epsilon=1e-5, name='backbone.bn1')(x)
     x = Activation('relu')(x)
 
+    # 272, 272, 64 -> 136, 136, 64
     x = ZeroPadding2D((1, 1))(x)
     x = MaxPooling2D((3, 3), strides=(2, 2), padding="valid")(x)
 
+    # 136, 136, 64 -> 136, 136, 256
     x = conv_block(x, 3, [64, 64, 256], strides=(1, 1), name="backbone.layers.0.0")
     x = identity_block(x, 3, [64, 64, 256], name="backbone.layers.0.1")
     x = identity_block(x, 3, [64, 64, 256], name="backbone.layers.0.2")
 
+    # 136, 136, 256 -> 68, 68, 512
     x = conv_block(x, 3, [128, 128, 512], name="backbone.layers.1.0")
     x = identity_block(x, 3, [128, 128, 512], name="backbone.layers.1.1")
     x = identity_block(x, 3, [128, 128, 512], name="backbone.layers.1.2")
     x = identity_block(x, 3, [128, 128, 512], name="backbone.layers.1.3")
     y1 = x
 
+    # 68, 68, 512 -> 34, 34, 1024
     x = conv_block(x, 3, [256, 256, 1024], name="backbone.layers.2.0")
     x = identity_block(x, 3, [256, 256, 1024], name="backbone.layers.2.1")
     x = identity_block(x, 3, [256, 256, 1024], name="backbone.layers.2.2")
@@ -76,6 +79,7 @@ def ResNet50(inputs):
     x = identity_block(x, 3, [256, 256, 1024], name="backbone.layers.2.5")
     y2 = x
 
+    # 34, 34, 1024 -> 17, 17, 2048
     x = conv_block(x, 3, [512, 512, 2048], name="backbone.layers.3.0")
     x = identity_block(x, 3, [512, 512, 2048], name="backbone.layers.3.1")
     x = identity_block(x, 3, [512, 512, 2048], name="backbone.layers.3.2")
